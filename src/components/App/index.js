20e392d3d1a4bea203f6d70731c7138399ad14f0
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2018-12-26 01:56:01 +0400

    implement the store from scratch

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+require('../../redux-parts')
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        ola
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/redux-parts/store.js
@@ -0,0 +1,49 @@
+const createStore = (reducer) => {
+     let state;
+     let listeners = [];
+
+     const getState = () => state;
+
+     const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+     };
+
+     const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener)
+        }
+     };
+
+     dispatch({});
+     return {getState, dispatch, subscribe};
+ }
+
+ const counter = (state = 0, action) => {
+     switch(action.type){
+         case 'INCREMENT':
+            return state + 1; 
+         case 'DECREMENT':
+            return state + 1; 
+         default:
+            return state;
+     }
+ }
+
+const store = createStore(counter);
+
+const subscriber = () => {
+    console.log("subscriber");
+}
+
+const unsib = store.subscribe(subscriber);
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+
+unsib();
+console.log(store.getState());
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
