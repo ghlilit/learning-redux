e20f52a035cc18c12b2d86c23da9d19de05df579
Lilit Ghandilyan <lilit_ghandilyan@edu.aua.am>
2018-12-27 14:38:24 +0400

    implement combineReducers

---
+++ src/redux-parts/combineReducers.js
@@ -0,0 +1,13 @@
+const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        Object.keys(reducers).reduce(
+            (nextState, key) => {
+                nextState[key] = reducers[key](
+                    state[key], action
+                );
+                return nextState;
+            },
+            {}
+        )
+    };
+}
